bc67e03272b7499ed5644971f08aac45e7c18baa
Rief Haalboom <superrief@hotmail.com>
2018-06-02 14:44:06 +0200

    Line Rider done

---
+++ Line Rider Prototype/Assets/LineManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour {
+
+	public GameObject linePrefab;
+
+	public GameObject normLine;
+	public GameObject boostLine;
+	public GameObject bounceLine;
+
+	public GameObject canvas;
+
+	Line activeLine;
+
+
+	public void Start()
+	{
+		linePrefab = normLine;
+	}
+	void Update()
+	{
+
+		if(canvas.activeInHierarchy == false){
+			if(Input.GetMouseButtonDown(0))
+			{
+				GameObject lineGO = Instantiate(linePrefab);
+				activeLine = lineGO.GetComponent<Line>();
+			}
+
+			if(Input.GetMouseButtonUp(0))
+			{
+				activeLine = null;
+			}
+
+			if(activeLine != null)
+			{
+				Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				activeLine.UpdateLine(mousePos);
+			}
+		}
+	}
+
+	public void BoostLine()
+	{
+		linePrefab = boostLine;
+	}
+
+	public void BounceLine()
+	{
+		linePrefab = bounceLine;
+	}
+}
---
+++ Line Rider Prototype/Assets/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+
+	public GameObject ui;
+	public GameObject openUIText;
+	public GameObject spaceToPlay;
+
+	void Start () 
+	{
+		ui.SetActive(false);
+	}
+	
+
+	void Update () 
+	{
+		UIUpdate();
+	}
+
+	void UIUpdate()
+	{
+		if(Input.GetButtonDown("Esc"))
+		{
+			if(ui.activeInHierarchy == false)
+			{
+				ui.SetActive(true);
+				openUIText.SetActive(false);
+			}
+			else
+			{
+				ui.SetActive(false);
+				openUIText.SetActive(true);
+			}
+		}
+	}
+	public void SpaceToPlay()
+	{
+		spaceToPlay.SetActive(false);
+
+	}
+}
