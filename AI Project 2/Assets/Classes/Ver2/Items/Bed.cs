4bc0a71becc8ddf2219c49540340457e37a6ec49
Rief Haalboom <superrieff@hotmail.com>
2018-06-26 15:52:18 +0200

    done

--- AI Project 2/Assets/Classes/Ver2/Items/Bed.cs
+++ AI Project 2/Assets/Classes/Ver2/Items/Bed.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Bed : BasicItem {
 
-	
-	public override void GiveEnergy(int value){
-		base.GiveEnergy(value);
-	}
+    bool canPlay;
+
+    public override void GiveEnergy(int value){
+        if (canPlay)
+        {
+            StartCoroutine(OverTime(value));
+        }
+    }
+
+    IEnumerator OverTime(int overTimeValue)
+    {
+		canPlay = false;
+        while(PlayerStats.instance.energy < 100)
+		{
+            PlayerStats.instance.energy += overTimeValue;
+            yield return new WaitForSeconds(1);
+		}
+        canPlay = true;
+    }
 }
